7f8b2ba487c1e4e263ecbbc0be3283c9d11dd99b
afflatus <afflatus@pathilorra.co.uk>
2015-11-23 22:12:07 +0000

    Add give_initial_stuff

---
+++ mods/give_initial_stuff/init.lua
@@ -0,0 +1,11 @@
+minetest.register_on_newplayer(function(player)
+	--print("on_newplayer")
+	if minetest.setting_getbool("give_initial_stuff") then
+		minetest.log("action", "Giving initial stuff to player "..player:get_player_name())
+		player:get_inventory():add_item('main', 'tool:spork')
+		player:get_inventory():add_item('main', 'equipment:torch 49')
+		player:get_inventory():add_item('main', 'plant:straw_bale 49')
+		player:get_inventory():add_item('main', 'material:twigs 49')
+	end
+end)
+
