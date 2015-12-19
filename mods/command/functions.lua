960dc9fb1fcf1ee90756e406b77d5e2b1d8d4f66
afflatus <afflatus@pathilorra.co.uk>
2015-12-19 13:57:22 +0000

    command: Add /welcome and introductory formspec on_newplayer.

--- mods/command/functions.lua
+++ mods/command/functions.lua
@@ -78,3 +78,74 @@ function command:make_jailor(adminname)
 	judge:setpos(prison_admin)
 	return true;
 end
+
+function command.welcome(player)
+	local target = player:get_player_name()
+	if not player then
+		return false, ("There's no player named '%s'."):format(target)
+	end
+	local fs = { }
+	local text = {"Welcome to Grail Test, "..target.."!",
+			"",
+			"Your journey begins in an otherworldly dark-age kingdom ",
+			"that stands on the borders between the lands of the ",
+			"living and the lands of the dead.",
+			"",
+			"Player killing isn't allowed but you have to survive against",
+			"monsters, using crafting, building and your ingenuity. ",
+			"Your  first task is to gather resources to build a shelter.",
+			"",
+			"Go find a tree and chop some wood, then you can make a ",
+			"pick and go mining. There is a playguide at: ",
+			"http://grailtest.pathilorra.co.uk/playguide.html",
+			"",
+			"Admins are called Wizards here. Hassling for privs or chat ",
+			"spamming will result in gagging or toading. Griefers will ",
+			"be arrested, probably without warning. Crafting, building ",
+			"great public buildings and helping other players is the ",
+			"best way to get player advancement and privs.",
+			"",
+			"If you have any problems /mail afflatus in-game or post in ",
+			"the server forum at: ",
+			"https://forum.minetest.net/viewtopic.php?f=10&t=13127.",}
+	text = table.concat(text, "\n")
+	text = minetest.formspec_escape(text)
+	table.insert(fs, "size[8,11]")
+	table.insert(fs, "background[0,0;8,11;ui_form_bg.png]")
+	table.insert(fs, "image[6.7,0.5;1,1;ui_icon.png]")
+	table.insert(fs, "image[6.7,2;1,2;character_1_preview.png]")
+	table.insert(fs, "image[6.7,4.5;1,1;tree_apple_tree_sapling.png]")
+	table.insert(fs, "image[6.7,6.5;1,1;tool_staff_ruby.png]")
+	table.insert(fs, "image[6.7,8.5;1,1;ui_craftguide_icon.png]")
+	table.insert(fs, "label[0.5,0.5;"..text.."]")
+	table.insert(fs, "button_exit[3,10.2;2,0.5;ok;OK]")
+	fs = table.concat(fs)
+	minetest.after(0.5, function()
+		minetest.show_formspec(target, "command:welcome", fs)
+	end)
+	return true
+end
+
+minetest.register_on_newplayer(function(player)
+	command.welcome(player)
+end)
+
+--[[
+minetest.register_on_joinplayer(function(player)
+	command.welcome(player)
+end)
+]]
+
+minetest.register_chatcommand("welcome", {
+	params = "",
+	privs = { },
+	description = "Show the welcome screen.",
+	func = function(name, params)
+		local player = minetest.get_player_by_name(name)
+		local ok, err = command.welcome(player)
+		if not ok then
+			return false, err
+		end
+		return true, "Hail o Valiant Grail Tester!"
+	end,
+})
