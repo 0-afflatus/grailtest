2f81f2fbf3c172a9f2f207ad681c188cc6ebc4a4
afflatus <afflatus@pathilorra.co.uk>
2016-04-07 10:15:23 +0100

    decor: Add mechanisms
    lever and pressure plate

--- mods/decor/handlers/nodeboxes.lua
+++ mods/decor/handlers/nodeboxes.lua
@@ -10,6 +10,23 @@ equipment.nodebox = {
 	null = { type = "fixed", fixed = { 0, 0, 0, 0, 0, 0 } }
 }
 
+equipment.pixelbox = function(size, boxes)
+	local fixed = {}
+	for _, box in pairs(boxes) do
+		-- `unpack` has been changed to `table.unpack` in newest Lua versions.
+		local x, y, z, w, h, l = unpack(box)
+		fixed[#fixed+1] = {
+			(x / size) - 0.5,
+			(y / size) - 0.5,
+			(z / size) - 0.5,
+			((x + w) / size) - 0.5,
+			((y + h) / size) - 0.5,
+			((z + l) / size) - 0.5
+		}
+	end
+	return { type = "fixed", fixed = fixed }
+end
+
 local mt = {}
 mt.__index = function(table, key)
 	local ref = equipment.box[key]
