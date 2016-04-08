ca3fb799aca2f0e38d10596f9f2834f48b804227
afflatus <afflatus@pathilorra.co.uk>
2016-04-08 12:21:38 +0100

    equipment: Add steel ladder

--- mods/equipment/ladder.lua
+++ mods/equipment/ladder.lua
@@ -37,3 +37,33 @@ minetest.register_craft({
 	burntime = 5,
 })
 
+minetest.register_node("equipment:ladder_steel", {
+	description = "Steel Ladder",
+	drawtype = "signlike",
+	tiles = {"equipment_ladder_steel.png"},
+	inventory_image = "equipment_ladder_steel.png",
+	wield_image = "equipment_ladder_steel.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	sunlight_propagates = true,
+	walkable = false,
+	climbable = true,
+	is_ground_content = false,
+	selection_box = {
+		type = "wallmounted",
+		--wall_top = = <default>
+		--wall_bottom = = <default>
+		--wall_side = = <default>
+	},
+	groups = {cracky = 2},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_craft({
+	output = 'equipment:ladder_steel 15',
+	recipe = {
+		{'mineral:steel_ingot', '', 'mineral:steel_ingot'},
+		{'mineral:steel_ingot', 'mineral:steel_ingot', 'mineral:steel_ingot'},
+		{'mineral:steel_ingot', '', 'mineral:steel_ingot'},
+	}
+})
