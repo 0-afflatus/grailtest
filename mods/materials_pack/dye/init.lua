877760005212bd6c3e1ab6190e0c37d759cec384
afflatus <afflatus@pathilorra.co.uk>
2022-03-29 18:04:08 +0100

    Updated dye colours

--- mods/materials_pack/dye/init.lua
+++ mods/materials_pack/dye/init.lua
@@ -9,6 +9,22 @@ dye.excolors = {"white", "lightgrey", "grey", "darkgrey", "black", "red", "orang
 
 dye.dyes = {
 	{"black",      "Black",      {dye=1, basecolor_black=1,   excolor_black=1,      unicolor_black=1},			"#333333"},
+	{"blue",       "Blue",       {dye=1, basecolor_blue=1,    excolor_blue=1,       unicolor_blue=1},			"#6666dd"},
+	{"brown",      "Brown",      {dye=1, basecolor_brown=1,   excolor_orange=1,     unicolor_dark_orange=1},	"#887755"},
+	{"cyan",       "Cyan",       {dye=1, basecolor_cyan=1,    excolor_cyan=1,       unicolor_cyan=1},			"#55bbcc"},
+	{"green",      "Green",      {dye=1, basecolor_green=1,   excolor_green=1,      unicolor_green=1},			"#448044"},
+	{"grey",       "Grey",       {dye=1, basecolor_grey=1,    excolor_grey=1,       unicolor_grey=1},			"#999999"},
+	{"magenta",    "Magenta",    {dye=1, basecolor_magenta=1, excolor_red_violet=1, unicolor_red_violet=1},		"#ee66ee"},
+	{"orange",     "Orange",     {dye=1, basecolor_orange=1,  excolor_orange=1,     unicolor_orange=1},			"#ee9966"},
+	{"pink",       "Pink",       {dye=1, basecolor_red=1,     excolor_red=1,        unicolor_light_red=1},		"#eebbdd"},
+	{"red",        "Red",        {dye=1, basecolor_red=1,     excolor_red=1,        unicolor_red=1},			"#bb6666"},
+	{"violet",     "Violet",     {dye=1, basecolor_magenta=1, excolor_violet=1,     unicolor_violet=1},			"#8866bb"},
+	{"white",      "White",      {dye=1, basecolor_white=1,   excolor_white=1,      unicolor_white=1},			"#eeeeee"},
+	{"yellow",     "Yellow",     {dye=1, basecolor_yellow=1,  excolor_yellow=1,     unicolor_yellow=1},			"#eecc00"},
+}
+
+--[[dye.dyes = {
+	{"black",      "Black",      {dye=1, basecolor_black=1,   excolor_black=1,      unicolor_black=1},			"#333333"},
 	{"blue",       "Blue",       {dye=1, basecolor_blue=1,    excolor_blue=1,       unicolor_blue=1},			"#6666ee"},
 	{"brown",      "Brown",      {dye=1, basecolor_brown=1,   excolor_orange=1,     unicolor_dark_orange=1},	"#997755"},
 	{"cyan",       "Cyan",       {dye=1, basecolor_cyan=1,    excolor_cyan=1,       unicolor_cyan=1},			"#66eeee"},
@@ -21,7 +37,7 @@ dye.dyes = {
 	{"violet",     "Violet",     {dye=1, basecolor_magenta=1, excolor_violet=1,     unicolor_violet=1},			"#cc66ff"},
 	{"white",      "White",      {dye=1, basecolor_white=1,   excolor_white=1,      unicolor_white=1},			"#ffffff"},
 	{"yellow",     "Yellow",     {dye=1, basecolor_yellow=1,  excolor_yellow=1,     unicolor_yellow=1},			"#ffdd33"},
-}
+}]]--
 
 function dye.get_color( nodename )
 	for _, row in ipairs( dye.dyes ) do
